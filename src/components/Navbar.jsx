ade7e1ef662b273cc5147505035659bbcb3a2093
Stefan Reeves <stefanreeves24@gmail.com>
2024-05-05 23:30:08 -0700

    final sprint1 navbar. styling not correct

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,25 +3,84 @@ import "../styles/componentstyles/Navbar.scss";
 import logo from "../assets/Icons/BrainFlix-logo.svg";
 import avatar from "../assets/Images/Mohan-muruge.jpg";
 import uploadIcon from "../assets/Icons/upload.svg";
+import searchIcon from "../assets/Icons/search.svg";
 
-function Navbar() {
+// function Navbar() {
+//     return (
+//       <nav className="navbar">
+//         <div className="navbar__logo">
+//           <img src={logo} alt="BrainFlix logo" />
+//         </div>
+//         <div className="navbar__search-upload-container">
+//           <div className="navbar_searchbox">
+//             <input type="text" className="navbar__search" placeholder="Search" />
+//           </div>
+//           <div className="navbar__upload">
+//             <button className="navbar__button">
+//             <img src={uploadIcon} alt="Upload Icon" className="navbar__icon"/>
+//             UPLOAD
+//             </button>
+//           </div>
+//         </div>
+
+//         <div className="navbar__profile">
+//           <img src={avatar} alt="profile avatar" className="navbar__avatar"/>
+//         </div>
+//       </nav>
+//     );
+//   }
+  
+//   export default Navbar;
+
+  // function Navbar() {
+  //   return (
+  //     <nav className="navbar">
+  //       <div className="navbar__logo">
+  //         <img src={logo} alt="BrainFlix logo" />
+  //       </div>
+  //       <div className="navbar__search-upload-container">
+  //         <input type="text" className="navbar__search" placeholder="Search" />
+  //         <div className="navbar__profile">
+  //         <button className="navbar__button">
+  //         <img src={uploadIcon} alt="Upload Icon" className="navbar__icon"/>
+  //         UPLOAD
+  //       </button>
+  //         </div>
+  //       </div>
+
+  //       <div className="navbar__profile">
+  //           <img src={avatar} alt="profile avatar" className="navbar__avatar"/>
+  //         </div>
+  //     </nav>
+  //   );
+  // }
+  
+  // export default Navbar;
+
+  function Navbar() {
     return (
       <nav className="navbar">
         <div className="navbar__logo">
           <img src={logo} alt="BrainFlix logo" />
         </div>
-        <div className="navbar__search-container">
-          <input type="text" className="navbar__search" placeholder="Search..." />
-          <div className="navbar__profile">
-            <img src={avatar} alt="profile avatar" className="navbar__avatar"/>
+
+          <div className="navbar__search-avatar">
+            <div className="navbar_searchbox">
+              <input type="text" className="navbar__search" placeholder="Search" />
+            </div>
+            <div className="navbar__profile">
+              <img src={avatar} alt="profile avatar" className="navbar__avatar"/>
+            </div>
           </div>
-        </div>
-        <button className="navbar__button">
-          <img src={uploadIcon} alt="Upload Icon" className="navbar__icon"/>
-          UPLOAD
-        </button>
+          <div className="navbar__upload">
+            <button className="navbar__button">
+              <img src={uploadIcon} alt="Upload Icon" className="navbar__icon"/>
+              UPLOAD
+            </button>
+          </div>
+
       </nav>
     );
-  }
-  
-  export default Navbar;
+}
+
+export default Navbar;
