d059e055686a9c025314ae2b143985b94ecc1ead
Stefan Reeves <stefanreeves24@gmail.com>
2024-05-05 19:05:10 -0700

    add timestamp

--- src/components/VideoHeroComments.jsx
+++ src/components/VideoHeroComments.jsx
@@ -4,12 +4,21 @@ import "../styles/componentstyles/VideoHeroComments.scss";
 function VideoHeroComments({ comments }) {
     return (
         <div className="comments">
-            {comments.map((comment) => (
-                <div key={comment.id} className="comments__comment">
-                    <h3 className="comments__name">{comment.name}</h3>
-                    <p className="comments__content">{comment.comment}</p>
-                </div>
-            ))}
+
+            {comments.map((comment) => {
+                const date = new Date(comment.timestamp);
+                const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+
+                return (
+                    <div key={comment.id} className="comments__comment">
+                        <div className="comments__header">
+                            <h3 className="comments__name">{comment.name}</h3>
+                            <p className="comments__timestamp">{formattedDate}</p>
+                        </div>
+                        <p className="comments__content">{comment.comment}</p>
+                    </div>
+                );
+            })}
         </div>
     );
 }
