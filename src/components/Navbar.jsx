3c8826982633c12a191dbd2534c5a94ca86cb3b1
Stefan Reeves <stefanreeves24@gmail.com>
2024-05-07 14:13:22 -0700

    improve responsive design with display none feature on mobile and desktop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,22 +63,22 @@ import searchIcon from "../assets/Icons/search.svg";
         <div className="navbar__logo">
           <img src={logo} alt="BrainFlix logo" />
         </div>
-
-          <div className="navbar__search-avatar">
-            <div className="navbar_searchbox">
-              <input type="text" className="navbar__search" placeholder="Search" />
-            </div>
-            <div className="navbar__profile">
-              <img src={avatar} alt="profile avatar" className="navbar__avatar"/>
-            </div>
+        <div className="navbar__content">
+          <div className="navbar__search">
+            <img src={searchIcon} alt="Search" className="navbar__search-icon" />
+            <input type="text" placeholder="Search" className="navbar__search-input" />
           </div>
-          <div className="navbar__upload">
-            <button className="navbar__button">
-              <img src={uploadIcon} alt="Upload Icon" className="navbar__icon"/>
-              UPLOAD
-            </button>
+          <div className="navbar__avatar-mobile">
+            <img src={avatar} alt="profile avatar"/>
           </div>
-
+        </div>
+        <div className="navbar__button">
+          <img src={uploadIcon} alt="Upload Icon" className="navbar__button-icon"/>
+          <span>UPLOAD</span>
+        </div>
+        <div className="navbar__avatar-desktop">
+            <img src={avatar} alt="profile avatar"/>
+        </div>
       </nav>
     );
 }
