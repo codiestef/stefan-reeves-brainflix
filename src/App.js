9515f0f5cd741fdb55f81d49bc28eaf39b75ef8c
Stefan Reeves <stefanreeves24@gmail.com>
2024-05-11 15:43:42 -0700

    delete uneccessary code and add dive tags for flex layout

--- src/App.js
+++ src/App.js
@@ -2,33 +2,21 @@
 import './App.css';
 
 import React, { useState } from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/aNavbar/Navbar';
 import VideoHero from './components/VideoHero';
 import VideoHeroAbout from './components/VideoHeroAbout';
+import VideoHeroAddComment from './components/VideoHeroAddComment'
 import VideoHeroComments from './components/VideoHeroComments';
 import VideosNextList from './components/VideosNextList';
 
 import videos from './data/videos.json';
 import videoDetails from './data/video-details.json';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navbar />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 function App() {
-  // State for the current video ID
   const [currentVideoId, setCurrentVideoId] = useState(videos[0].id);
 
-  // Find the current video details
   const currentVideo = videoDetails.find(video => video.id === currentVideoId);
 
-  // Handle video selection from the sidebar
   const handleVideoSelect = (id) => {
       setCurrentVideoId(id);
   };
@@ -37,13 +25,16 @@ function App() {
       <div className="app">
           <Navbar />
           <VideoHero video={currentVideo} />
+          <body>
           <div className="VideoSelectAndNextDisplay">
             <div className="VideoHeroAboutCommentsCombo">
               <VideoHeroAbout video={currentVideo} />
+              <VideoHeroAddComment commentsCount={currentVideo.comments.length} />
               <VideoHeroComments comments={currentVideo.comments} />
             </div>
             <VideosNextList videos={videos} currentVideoId={currentVideoId} onVideoSelect={handleVideoSelect} />
           </div>
+          </body>
       </div>
   );
 }
