c402941e2300efbe3cd7b45d227c34a758b4fe03
Stefan Reeves <stefanreeves24@gmail.com>
2024-05-13 20:29:06 -0700

    add link tags from react-router-dom, replacing button tag, with additional link tag on logo going to '/'

--- src/components/aNavbar/Navbar.jsx
+++ src/components/aNavbar/Navbar.jsx
@@ -1,43 +1,17 @@
 import React from 'react';
-import "../aNavbar/Navbar.scss";
+import { Link } from "react-router-dom";
 import logo from "../../assets/Icons/BrainFlix-logo.svg";
-import avatar from "../../assets/Images/Mohan-muruge.jpg";
-import uploadIcon from "../../assets/Icons/upload.svg";
 import searchIcon from "../../assets/Icons/search.svg";
-
-//   function Navbar() {
-//     return (
-//       <nav className="navbar">
-//         <div className="navbar__logo">
-//           <img src={logo} alt="BrainFlix logo" />
-//         </div>
-//         <div className="navbar__content">
-//           <div className="navbar__search">
-//             <img src={searchIcon} alt="Search" className="navbar__search-icon" />
-//             <input type="text" placeholder="Search" className="navbar__search-input" />
-//           </div>
-//           <div className="navbar__avatar-mobile">
-//             <img src={avatar} alt="profile avatar"/>
-//           </div>
-//         </div>
-//         <div className="navbar__button">
-//           <img src={uploadIcon} alt="Upload Icon" className="navbar__button-icon"/>
-//           <span>UPLOAD</span>
-//         </div>
-//         <div className="navbar__avatar-desktop">
-//             <img src={avatar} alt="profile avatar"/>
-//         </div>
-//       </nav>
-//     );
-// }
-
-// export default Navbar;
+import avatar from "../../assets/Images/Mohan-muruge.jpg";
+import "../aNavbar/Navbar.scss";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar__logo">
-        <img src={logo} alt="BrainFlix logo" />
+        <Link to="/">
+          <img src={logo} alt="BrainFlix logo" />
+        </Link>
       </div>
       <div className="navbar__content">
         <div className="navbar__search">
@@ -48,13 +22,11 @@ function Navbar() {
           <img src={avatar} alt="profile avatar"/>
         </div>
       </div>
-      <button
-        className="navbar__button"
-        type="submit">
-        UPLOAD
-      </button>
+      <Link to="/upload" className="navbar__button">
+        <span>UPLOAD</span>
+      </Link>
       <div className="navbar__avatar-desktop">
-          <img src={avatar} alt="profile avatar"/>
+        <img src={avatar} alt="profile avatar"/>
       </div>
     </nav>
   );
