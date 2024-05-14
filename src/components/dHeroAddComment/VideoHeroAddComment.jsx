c860a3ff8f4d2470d19721e65be27a1e41e99873
Stefan Reeves <stefanreeves24@gmail.com>
2024-05-13 20:45:46 -0700

    fix missing 'Name' in 'classNames'

--- src/components/dHeroAddComment/VideoHeroAddComment.jsx
+++ src/components/dHeroAddComment/VideoHeroAddComment.jsx
@@ -1,33 +1,29 @@
 import React from 'react';
 import '../dHeroAddComment/VideoHeroAddComment.scss';
-import commentIcon from '../../assets/Icons/add_comment.svg';
 
 function VideoHeroAddComment({ commentsCount }) {
+
   return (
     <div className="add-comment">
-        <h3 class="subheader">{commentsCount} Comments</h3>
+        <h3 className="subheader">{commentsCount} Comments</h3>
 
-    <div>
-    <form className="comment-form">
-        <div className="comment-form__labelAndText">
-        <label class="comment-form__label">
-            JOIN THE CONVERSATION
-        </label>
-        <textarea
-            class="comment-form__textarea"
-            type="text"
-            placeholder="Add a new comment"
-        />
-        </div>
-
+        <form className="comment-form">
+            <div className="comment-form__labelAndText">
+                <label className="comment-form__label">
+                    JOIN THE CONVERSATION
+                </label>
+                <textarea
+                    className="comment-form__textarea"
+                    type="text"
+                    placeholder="Add a new comment"
+                />
+            </div>
             <button
                 className="comment-form__button"
                 type="submit">
                 COMMENT
             </button>
-
         </form>
-    </div>
     </div>
   );
 }
