6ebe8f0be029b386d3290c81e4a8ec79e8210f06
Stefan Reeves <stefanreeves24@gmail.com>
2024-05-13 21:28:14 -0700

    add BrowserRouter, Routes, and Route functionality

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
-// import Navbar from './components/Navbar';
-import './App.css';
-
-import React, { useState } from 'react';
-import Navbar from './components/aNavbar/Navbar.jsx';
-import VideoHero from './components/bHeroVideo/VideoHero.jsx';
-import VideoHeroAbout from './components/cAboutVideo/VideoHeroAbout.jsx';
-import VideoHeroAddComment from './components/dHeroAddComment/VideoHeroAddComment.jsx'
-import VideoHeroComments from './components/eHeroComments/VideoHeroComments.jsx';
-import VideosNextList from './components/fNextVideosList/VideosNextList.jsx';
-
-import videos from './data/videos.json';
-import videoDetails from './data/video-details.json';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../src/components/aNavbar/Navbar";
+import VideoDetailPage from "./pages/VideoDetailsPage/VideoDetailsPage";
+import UploadPage from "./pages/VideoUploadPage/UploadPage";
+import './App.scss';
 
 function App() {
-  const [currentVideoId, setCurrentVideoId] = useState(videos[0].id);
-
-  const currentVideo = videoDetails.find(video => video.id === currentVideoId);
-
-  const handleVideoSelect = (id) => {
-      setCurrentVideoId(id);
-  };
-
   return (
-      <div className="app">
-          <Navbar />
-          <VideoHero video={currentVideo} />
-          <body>
-          <div className="VideoSelectAndNextDisplay">
-            <div className="VideoHeroAboutCommentsCombo">
-              <VideoHeroAbout video={currentVideo} />
-              <VideoHeroAddComment commentsCount={currentVideo.comments.length} />
-              <VideoHeroComments comments={currentVideo.comments} />
-            </div>
-            <VideosNextList videos={videos} currentVideoId={currentVideoId} onVideoSelect={handleVideoSelect} />
-          </div>
-          </body>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<VideoDetailPage />} />
+          <Route path="/videos/:id" element={<VideoDetailPage />} />
+          <Route path="/upload" element={<UploadPage />} />
+        </Routes>
       </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-
